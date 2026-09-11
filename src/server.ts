d491dc4c1a54962645997988a128136352e33e7d
mohit-bhandari45 <mohitbhandari852@gmail.com>
2026-09-11 16:26:51 +0530

    fix(cors): add Authorization to Access-Control-Allow-Headers

--- src/server.ts
+++ src/server.ts
@@ -27,7 +27,7 @@ if (!WEBHOOK_SECRET) {
 
 app.use((_req: Request, res: Response, next: NextFunction) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     if (_req.method === "OPTIONS") return res.sendStatus(200);
     next();
